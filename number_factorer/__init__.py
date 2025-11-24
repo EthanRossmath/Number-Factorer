02fede0a662f3ed23fc4118de82c78474b2fc861
EthanRossmath <ethanross981@gmail.com>
2025-11-23 22:38:18 -0500

    made the repo into a library

---
+++ number_factorer/__init__.py
@@ -0,0 +1,20 @@
+
+from .Factor_Number import (
+    Number_Factorer,
+    IncrementOrder,
+    BabyGiantOrder,
+    ShorFactorization,
+    EkeraFactorization,
+    ShorOrder,
+    BeauregardOrder
+)
+
+__all__ = [
+    "Number_Factorer",
+    "IncrementOrder",
+    "BabyGiantOrder",
+    "ShorFactorization",
+    "EkeraFactorization",
+    "ShorOrder",
+    "BeauregardOrder",
+]
